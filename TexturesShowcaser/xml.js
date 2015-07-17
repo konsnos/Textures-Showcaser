135980239f1554333f04a33bb6782737cbce0185
Konstantinos Egkarchos <konsnosl@gmail.com>
2015-07-17 09:22:19 +0300

    Moved some javascript functions to external file.

---
+++ TexturesShowcaser/xml.js
@@ -0,0 +1,99 @@
+var xmlDomDoc;
+
+function InitializeXMLNodes()
+{
+	// create the nodeType constants if the Node object is not defined
+	if (!window.Node)
+	{
+		var Node =
+		{
+			ELEMENT_NODE                :  1,
+			ATTRIBUTE_NODE              :  2,
+			TEXT_NODE                   :  3,
+			CDATA_SECTION_NODE          :  4,
+			ENTITY_REFERENCE_NODE       :  5,
+			ENTITY_NODE                 :  6,
+			PROCESSING_INSTRUCTION_NODE :  7,
+			COMMENT_NODE                :  8,
+			DOCUMENT_NODE               :  9,
+			DOCUMENT_TYPE_NODE          : 10,
+			DOCUMENT_FRAGMENT_NODE      : 11,
+			NOTATION_NODE               : 12
+		}
+	}
+}
+
+function checkXMLValidity(xmlFile)
+{
+	if (xmlFile && xmlFile.type == "text/xml")
+	{
+		readXML(xmlFile);
+	}
+	else
+	{
+		alert("Failed to load file.");
+	}
+}
+
+function readXML(xmlFile)
+{
+	var reader = new FileReader();
+	waitForTextReadComplete(reader);
+	reader.readAsText(xmlFile);
+}
+
+function waitForTextReadComplete(reader)
+{
+    reader.onloadend = function(event)
+    {
+        var text = event.target.result;
+
+        parseTextAsXml(text);
+    }
+}
+
+function parseTextAsXml(text)
+{
+    var parser = new DOMParser();
+    var xmlDomDoc = parser.parseFromString(text, "text/xml");
+    
+	var event = new CustomEvent('xmlLoaded', {'detail': xmlDomDoc});
+	document.dispatchEvent(event);
+    //xmlLoadedSuccessfully();
+}
+
+/** Test function to traverse the whole xml node tree. */
+function traverse(tree) 
+{
+	if(tree.hasChildNodes()) 
+	{ 
+		document.write('<ul><li>');
+		document.write('<b>' + tree.tagName + '</b>');
+		var nodes=tree.childNodes.length;
+		for(var i=0; i<tree.childNodes.length; i++)
+			traverse(tree.childNodes[i]);
+		document.write('</li></ul>'); 
+	}
+}
+
+/** Finds inside a node the required name from the attributes. */
+function findName (node, name) 
+{
+	var childNodes = node.childNodes;
+	for (var i = 0; i < childNodes.length; i++)
+	{
+		// Check if node and if folder.
+		if (childNodes[i].nodeType == Node.ELEMENT_NODE && childNodes[i].nodeName == "folder")
+		{
+			// Check if it is the name we are searching for
+			if (childNodes[i].getAttribute('name') == name)
+			{
+				// return it
+				return childNodes[i];
+			}
+		}
+	}
+
+	console.log("Houston, we have a problem");
+	return null;
+}
